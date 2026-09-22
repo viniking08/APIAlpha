a1cdc5d2c6a805706c0ee95c5d940393805c6e18
viniking08 <vinitransformers@hotmail.com>
2026-09-21 21:50:26 -0300

    base

--- backend/server.js
+++ backend/server.js
@@ -20,12 +20,41 @@ const produtos = [
 
 app.use(express.json());
 
-/*app.get("/". () =>{
-
+app.get("/teste", (req, res) => {
+    res.send("rota teste f")
 });
 
-app.post();
+app.get("/produtos", (req, res) => {
+    res.json(produtos)
+});
 
+app.get("/produtos/:id", (req, res) => {
+    const id = req.params.id;
+    const produto = produtos.find(produto => produto.id == id);
+
+    if(!produto) {
+        return res.status(404).json({
+            mensagem: "Produto não encontrado"
+        })
+    }
+    res.json(produto);
+});
+
+app.post("/produtos", (req, res) => {
+    console.log("achou rota")
+    const novoProduto = {
+        id: produtos.length + 1,
+        nome: req.body.nome,
+        marca: req.body.marca,
+        preco: req.body.preco
+    }
+
+    produtos.push(novoProduto);
+    res.status(201).json(novoProduto);
+});
+
+
+/*
 app.put();
 
 app.delete();
